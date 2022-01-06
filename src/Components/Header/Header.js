6e1ae285ed7502b5bc86a848641d1c1ba6eb7f9e
eduairet <hola@eduairet.com>
2022-01-05 20:31:19 -0600

    Update Header
    canvas animation

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,30 +1,42 @@
-import './Header.css';
-import { useEffect } from 'react';
+import "./Header.css";
+import { useEffect } from "react";
 
 function Header() {
-
-  useEffect( () => {
-    const c = document.getElementById('header-triangle');
+  const headerAnimation = () => {
+    const now = new Date();
+    const speed = now.getMilliseconds() * .02;
+    const amplitude = 3;
+    const movex = Math.sin(speed)*amplitude;
+    const movey = Math.cos(speed)*amplitude;
+    const c = document.getElementById("header-triangle");
     const ctx = c.getContext("2d");
-    ctx.moveTo(0, c.height);
-    ctx.lineTo(c.width*.5, 0);
-    ctx.lineTo(c.width, c.height);
-    ctx.lineTo(0, c.height);
-    ctx.strokeStyle = 'white';
+    ctx.clearRect(0, 0, c.width, c.height);
+    ctx.beginPath();
+    ctx.moveTo(c.width*.1, c.height*.9 - movey);
+    ctx.lineTo(c.width * 0.5, c.height*.1 + movey);
+    ctx.lineTo(c.width*.9 - movex, c.height*.9 - movey);
+    ctx.lineTo(c.width*.1, c.height*.9 - movey);
+    ctx.strokeStyle = "white";
     ctx.lineWidth = 4;
     ctx.stroke();
+    window.requestAnimationFrame(headerAnimation);
+  };
+
+  useEffect(() => {
+    window.requestAnimationFrame(headerAnimation);
   });
 
   return (
-    <header id='app-header' className="App-header">
-        <div className='header-title'>
-            <h3>ANÁHUAC <span className='heavy'>5.0</span></h3>
-            <h4 className='manifiesto orange'>Manifiesto del futuro</h4>
-        </div>
-        <canvas id="header-triangle"></canvas>
+    <header id="app-header" className="App-header">
+      <div className="header-title">
+        <h3>
+          ANÁHUAC <span className="heavy">5.0</span>
+        </h3>
+        <h4 className="manifiesto orange">Manifiesto del futuro</h4>
+      </div>
+      <canvas id="header-triangle"></canvas>
     </header>
   );
-
 }
 
 export default Header;
