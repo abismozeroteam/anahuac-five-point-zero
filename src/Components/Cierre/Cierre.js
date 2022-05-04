b7d0f7b702f751362cbeccce8cf869d140f9d91d
eduairet <hola@eduairet.com>
2022-05-03 19:59:32 -0500

    Update Cierre.js

--- src/Components/Cierre/Cierre.js
+++ src/Components/Cierre/Cierre.js
@@ -4,6 +4,8 @@ function Cierre() {
     return (
         <section id="cierre" className='section-container'>
             <h3>¿Listo para el futuro?</h3>
+            <hr/>
+            <p>Maracuyá Atelier 2022 ©</p>
         </section>
     )
 }
